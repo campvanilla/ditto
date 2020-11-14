b48668ce00db9eba50e84c0e1c4afa5f7d3ff377
Abinav Seelan <abinav.n.seelan@gmail.com>
2020-11-14 21:13:45 +0530

    fix: GH Actions

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: !!process.env.COVERAGE || process.env.CI,
+  collectCoverage: !!process.env.COVERAGE || process.env.CI === 'true',
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',

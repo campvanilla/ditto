21443b5a3134b705d47b92151d853eae2eb3a841
Aditi Mohanty <rheaditi@users.noreply.github.com>
2020-11-15 15:54:10 +0530

    chore: use fallback colors

--- src/converter.js
+++ src/converter.js
@@ -1,25 +1,39 @@
 const json5 = require('json5');
 
-const { readFile } = require('./utils/promises');
-const { ANSI_COLOR_FROM_VSCODE: VSCODE_THEME_KEY_TO_ANSI_COLOR } = require('./constants');
-const { hexToRGBA } = require('./utils/color');
 const { iterm } = require('./utils/xml');
+const { hexToRGBA } = require('./utils/color');
+const { readFile } = require('./utils/promises');
+const { ANSI_COLOR_FROM_VSCODE: VSCODE_THEME_KEY_TO_ANSI_COLOR, DEFAULT_FALLBACK_COLORS } = require('./constants');
 
 const readTheme = async (path) => {
   const contents = (await readFile(path)).toString();
   return json5.parse(contents);
 };
 
-exports.convertTheme = async ({ path }) => {
-  const { colors } = await readTheme(path);
+const getItermColor = ({ themeType, name, vscodeTheme }) => {
+  const possibleKeys = VSCODE_THEME_KEY_TO_ANSI_COLOR[name];
+  const vscodeColor = possibleKeys.find((color) => !!vscodeTheme[color]);
 
-  const ansiColors = Object.entries(VSCODE_THEME_KEY_TO_ANSI_COLOR).map(([ansiKey, possibleColorKeys]) => {
-    const colorKey = possibleColorKeys.find((key) => !!colors[key]);
+  if (vscodeColor) {
+    return vscodeTheme[vscodeColor];
+  }
+  const fallback = DEFAULT_FALLBACK_COLORS[themeType][name];
+  console.warn(`The theme is missing a color for ${name}, using fallback for ${themeType} themes ${fallback}`);
+  return fallback;
+};
+
+exports.convertTheme = async ({ path }) => {
+  const { colors: vscodeTheme, type: themeType } = await readTheme(path);
+
+  const itermColors = Object.keys(VSCODE_THEME_KEY_TO_ANSI_COLOR).map((name) => {
+    const colorHex = getItermColor({ name, themeType, vscodeTheme });
+    const colorRBA = hexToRGBA(colorHex);
+
     return {
-      key: ansiKey,
-      ...hexToRGBA(colors[colorKey]),
+      key: name,
+      ...colorRBA,
     };
   });
 
-  return iterm.getThemeXML(ansiColors);
+  return iterm.getThemeXML(itermColors);
 };
